a9167a880778b81577adc8f6acee0462376a1240
Muhammed <coolzyte@gmail.com>
2023-04-08 18:08:07 +0100

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,84 @@
+import Header from '../components/header/Header';
+
+const HomePage = () => {
+  return (
+    <div className='main-left theme-dark'>
+      <Header />
+
+      {/* <Slider />
+     
+
+      <About />
+     
+
+      <Resume />
+      
+      <section id="work" className="section theme-light dark-bg">
+        <div className="container">
+          <div className="title">
+            <h3>My Portfolio.</h3>
+          </div>
+          <Portfolio />
+        </div>
+      </section>
+     
+
+      <section id="blog" className="section">
+        <div className="container">
+          <div className="title">
+            <h3>Latest Blog.</h3>
+          </div>
+          <Blog />
+        </div>
+      </section>
+     
+
+      <section id="contactus" className="section theme-light dark-bg">
+        <div className="container">
+          <div className="row">
+            <div
+              className="col-lg-5 col-xl-4 m-15px-tb"
+              data-aos="fade-right"
+              data-aos-duration="1200"
+            >
+              <ContactInfo />
+            </div>
+          
+
+            <div
+              className="col-lg-7 ml-auto m-15px-tb"
+              data-aos="fade-right"
+              data-aos-duration="1200"
+              data-aos-delay="200"
+            >
+              <div className="contact-form">
+                <h4>Say Something</h4>
+                <Contact />
+              </div>
+            </div>
+           
+
+            <div
+              className="col-12"
+              data-aos="fade-up"
+              data-aos-duration="1200"
+              data-aos-delay="300"
+            >
+              <Map />
+            
+            </div>
+          
+          </div>
+        </div>
+      </section>
+      
+
+      <footer className="footer white">
+        <div className="container">
+          <Footer />
+        </div>
+      </footer> */}
+    </div>
+  );
+};
+export default HomePage;
